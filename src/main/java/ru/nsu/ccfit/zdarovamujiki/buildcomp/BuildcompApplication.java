57672bc5499ad64fde068b393e5a89bd541cab09
ZdarovaMujiki <vvremezovv@mail.ru>
2022-04-21 21:47:13 +0700

    initial commit

---
+++ src/main/java/ru/nsu/ccfit/zdarovamujiki/buildcomp/BuildcompApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.ccfit.zdarovamujiki.buildcomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildcompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildcompApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nsu/ccfit/zdarovamujiki/buildcomp/BuildcompApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.ccfit.zdarovamujiki.buildcomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildcompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
